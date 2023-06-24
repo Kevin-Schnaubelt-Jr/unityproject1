8df59a7bab49ea249f4f381977b56cf107c7a9b4
Kevin Schnaubelt Jr <kevinschnaubelt@gmail.com>
2023-06-24 14:44:11 -0400

    update

---
+++ Assets/PrintToConsole.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(@"
+        Hello World!
+        Also, why is my gun all melty?
+        ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
